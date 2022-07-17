8a1cf7f3b63e44d47557ad2291ce0ad470907d4d
Draganovski Matej <draganovski.matej@gmail.com>
2022-07-17 17:08:42 +0200

    Created Transactions import API

---
+++ PersonalFinanceManagement/Models/TransactionsModel.cs
@@ -0,0 +1,15 @@
+﻿namespace PFM.Backend.Models
+{
+    public class TransactionsModel
+    {
+        public string Id { get; set; }
+        public string BeneficiaryName { get; set; }
+        public string Date { get; set; }
+        public string Direction { get; set; }
+        public string Amount { get; set; }
+        public string Description { get; set; }
+        public string Currency { get; set; }
+        public string MCC { get; set; }
+        public string Kind { get; set; }
+    }
+}
---
+++ PersonalFinanceManagement/Services/TransactionMapper.cs
@@ -0,0 +1,21 @@
+﻿using CsvHelper.Configuration;
+using PFM.Backend.Models;
+
+namespace PFM.Backend.Services
+{
+    public class TransactionMapper : ClassMap<TransactionsModel>
+    {
+        public TransactionMapper()
+        {
+            Map(m => m.Id).Name("id");
+            Map(m => m.BeneficiaryName).Name("beneficiary-name");
+            Map(m => m.Date).Name("date");
+            Map(m => m.Direction).Name("direction");
+            Map(m => m.Amount).Name("amount");
+            Map(m => m.Description).Name("description");
+            Map(m => m.Currency).Name("currency");
+            Map(m => m.MCC).Name("mcc");
+            Map(m => m.Kind).Name("kind");
+        }
+    }
+}
